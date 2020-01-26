3d23c9d6ef26d994bc46d4a67178fb8175051bc3
jekifernandes <59949495+jekifernandes@users.noreply.github.com>
2020-01-26 22:26:13 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> -Jeki Fernandes -Muhammad Muhiddin -Suli Noprianfika 
+           
+        </div>
+    </div>
+</div>
